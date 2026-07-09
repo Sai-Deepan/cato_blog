cc9f9f5bf74aa5480c7b8d878ae46cde17f76f4c
Sai-Deepan <skdeepan.sai@gmail.com>
2026-07-10 00:15:05 +0530

    New Feature: Added Know more button in /about/ and fix experience section

---
+++ static/js/know-more.js
@@ -0,0 +1,41 @@
+const sidebar = document.getElementById("aboutSidebar");
+const overlay = document.getElementById("sidebarOverlay");
+
+const heroLeft = document.querySelector(".about-left");
+const heroRight = document.querySelector(".about-right");
+
+document
+    .getElementById("knowMoreBtn")
+    .addEventListener("click", openSidebar);
+
+document
+    .getElementById("closeSidebar")
+    .addEventListener("click", closeSidebar);
+
+overlay.addEventListener("click", closeSidebar);
+
+function openSidebar() {
+
+    sidebar.classList.add("open");
+    overlay.classList.add("show");
+
+    gsap.to([heroLeft, heroRight], {
+        x: -30,
+        duration: 0.45,
+        ease: "power3.out"
+    });
+
+}
+
+function closeSidebar() {
+
+    sidebar.classList.remove("open");
+    overlay.classList.remove("show");
+
+    gsap.to([heroLeft, heroRight], {
+        x: 0,
+        duration: 0.45,
+        ease: "power3.out"
+    });
+
+}
